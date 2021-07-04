11e084f4c4654018022d539452983a131967d31a
Chewzhiyan <zhiyannnn@gmail.com>
2021-07-05 01:51:04 +0800

    Delete

--- script.js
+++
@@ -1,7 +0,0 @@
-let add_date = document.getElementById('add-date');
-let add_amt = document.getElementById('add-amount');
-let add_desc = document.getElementById('add-desc');
-
-function addtrans(){
-    
-}
